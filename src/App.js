47dc13be63397c577de222d65ed58374de8373ab
January <januaryobrien.ux@gmail.com>
2020-08-26 20:39:24 -0400

    todo list and profile img added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
 import React from 'react';
 import './App.scss';
 
+
+
+const todoList = [{
+  title: "Pick up clothes from cleaners",
+  description: "your description",
+  completed: false
+},
+{
+  title: "Order groceries for the week",
+  description: "your description",
+  completed: false
+},
+{
+  title: "Clean out garage",
+  description: "your description",
+  completed: true
+}];
+const profile = {
+  user: "January",
+  profileImg: "https://www.fillmurray.com/640/360"
+};
+
 function App() {
   return (
+
       <div className="App">
       <h1>ToDo App</h1>
-      <div className="task-items">
-      {/* <img className="check" alt="check-button" src="./check.jpg"/> */}
-      <input type="checkbox" checked></input>
-      <input type="text"></input>
-      {/* <form id ="to-do-form">
-        <input type="text" placeholder= "Enter Task"/>
-        <button type= "submit">Add</button>
-        <img alt="trash-logo" src="./trash.jpg"/>
-        <input type="text" placeholder= "Enter Task"/>
-        <button type= "submit">Add</button>
-        <img alt="trash-logo" src="./trash.jpg"/>
-        <input type="text" placeholder= "Enter Task"/>
-        <button type= "submit">Add</button>
-        <img alt="trash-logo" src="./trash.jpg"/>
-        <input type="text" placeholder= "Enter Task"/>
-        <button type= "submit">Add</button>
-        <img alt="trash-logo" src="./trash.jpg"/>
-      </form> */}
-      </div>
-      {/* <div className="bottomnav"></div> */}
-      <div className="list"><img alt="list=logo" src="./list.png"/></div>
-      <div className="calendar"><img alt="calendar-logo" src="./Calendar.png"/></div>
-      <div className="setting"><img alt="setting-logo" src = "./settings.png"/></div>
+          <div className="user-info">
+            {profile.user}
+            <img alt="bill" src={profile.profileImg}/>
+          </div>
+      <ul>
+        {todoList.map((todo) => {
+          return (
+            <li>
+              <p>{todo.title}</p>
+              <p>{todo.description}</p>
+              <input type="checkbox" checked={todo.completed}/>
+            </li>
+          );
+        })}
+      </ul>
+        <div className="bottom-nav">
+        <div className="list"><img alt="list=logo" src="./list.png"/></div>
+        <div className="calendar"><img alt="calendar-logo" src="./Calendar.png"/></div>
+         <div className="setting"><img alt="setting-logo" src = "./settings.png"/></div>
+       </div>
     </div>
   );
 }
